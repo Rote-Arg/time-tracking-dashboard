9f2b5454e19c6d465fb13204fb5878498da98f88
Rote-Arg <elrote@gmail.com>
2022-03-23 10:30:10 -0300

    empezamos un poco a tocar el js pero no terminamos de cambiar estilos segun el button que haya sido marcado

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-let daily = document.querySelectorAll("div.daily");
-let modifyDaily = daily.map(element => element.display('none'))
-('none'); 
-console.log(modifyDaily);
-let weekly = document.querySelectorAll("div.weekly");
-console.log(weekly)
-let monthly = document.querySelectorAll("div.monthly");
-console.log(monthly)
+let dailyClass = () => {
+    let daily = document.querySelectorAll("div.daily");
+    for(let i = 0; i < daily.length; i++) {
+        daily[i].className= 'none';
+    }
+}
+
+let weeklyClass = () => {
+    let weekly = document.querySelectorAll("div.weekly");
+    for(let i = 0; i < daily.length; i++) {
+        weekly[i].className= 'none';
+    }
+}
+
+let monthlyClass = () => {
+    let monthly = document.querySelectorAll("div.monthly");
+    for(let i = 0; i < daily.length; i++) {
+        monthly[i].className= 'none';
+    }
+}
+
+let activate = value => {
+    let nav = document.querySelector("nav")
+    console.log(nav)
+
+}
