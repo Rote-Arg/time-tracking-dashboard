57772b6dd7524eeeb1136633221f9a8ff77dbd91
Rote-Arg <elrote@gmail.com>
2022-03-23 01:31:16 -0300

    Tenemos que manejar el DOM con el nav

---
+++ main.js
@@ -0,0 +1,8 @@
+let daily = document.querySelectorAll("div.daily");
+let modifyDaily = daily.map(element => element.display('none'))
+('none'); 
+console.log(modifyDaily);
+let weekly = document.querySelectorAll("div.weekly");
+console.log(weekly)
+let monthly = document.querySelectorAll("div.monthly");
+console.log(monthly)
