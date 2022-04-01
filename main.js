e90dd9e2e0502d6eb7de96b4469c28ba3080fa4b
Rote-Arg <elrote@gmail.com>
2022-04-01 16:05:05 -0300

    anda el filtro por daily, weekly y monthly

--- main.js
+++ main.js
@@ -19,8 +19,47 @@ let monthlyClass = () => {
     }
 }
 
-let activate = value => {
-    let nav = document.querySelector("nav")
-    console.log(nav)
 
+let activate = element => {
+
+    let elemento = element.target.classList
+    elemento.length == 0 ? elemento.add("activate") : null;
+
+
+
+    let condition= element.target.innerHTML
+    let daily = document.querySelectorAll(".daily" || ".daily-none")
+    let weekly = document.querySelectorAll(".weekly" || ".weekly-none")
+    let monthly = document.querySelectorAll(".monthly" || ".monthly-none")
+    let titleDayli  = document.querySelector("#titleDaily")
+    let titleWeekly =   document.querySelector("#titleWeekly")
+    let titleMonthly =  document.querySelector("#titleMonthly")
+
+
+    switch(condition) {
+        case "Daily":
+            daily.forEach(day => day.classList.length == 2 ? day.classList.remove('none') : null)
+            monthly.forEach(month => month.classList.length !== 2 ? month.classList.add('none') : null)
+            weekly.forEach(week => week.classList.length !== 2 ? week.classList.add('none') : null)
+            titleDayli.classList.length == 0 ? titleDayli.classList.add('activate') : null
+            titleWeekly.classList.length !== 0 ? titleWeekly.classList.remove('activate') : null
+            titleMonthly.classList.length !== 0 ? titleMonthly.classList.remove('activate') : null
+            break
+        case "Weekly":
+            weekly.forEach(week =>  week.classList.length == 2 ? week.classList.remove('none') : null)
+            monthly.forEach(month => month.classList.length !== 2 ? month.classList.add('none') : null)
+            daily.forEach(day => day.classList.length !== 2 ? day.classList.add('none') : null)
+            titleWeekly.classList.length == 0 ? titleWeekly.classList.add('activate') : null
+            titleDayli.classList.length !== 0 ? titleDayli.classList.remove('activate') : null
+            titleMonthly.classList.length !== 0 ? titleMonthly.classList.remove('activate') : null
+            break
+        case "Monthly":
+            monthly.forEach(month => month.classList.length == 2 ? month.classList.remove('none') : null)
+            daily.forEach(day => day.classList.length !== 2 ? day.classList.add('none') : null)
+            weekly.forEach(week => week.classList.length !== 2 ? week.classList.add('none') : null)
+            titleMonthly.classList.length == 0 ? titleMonthly.classList.add('activate') : null
+            titleDayli.classList.length !== 0 ? titleDayli.classList.remove('activate') : null
+            titleWeekly.classList.length !== 0 ? titleWeekly.classList.remove('activate') : null
+            break
+    }
 }
